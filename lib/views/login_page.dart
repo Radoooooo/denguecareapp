c8b71b0e08f132c8c17c63a3c464d2f368302b9b
Radoooooo <radooo1235@gmail.com>
2023-08-10 18:40:49 +0800

    edited login page

--- lib/views/login_page.dart
+++ lib/views/login_page.dart
@@ -1,3 +1,4 @@
+import 'package:denguecare/views/widgets/input_widget.dart';
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatefulWidget {
@@ -8,8 +9,41 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    var size = MediaQuery.of(context).size.width;
+    return Scaffold(
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(
+            horizontal: 20,
+          ),
+          child: Card(
+            elevation: 8,
+            child: Container(
+              padding: const EdgeInsets.all(24),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text("Login Page"),
+                  const SizedBox(height: 20),
+                  InputWidget(
+                      hintText: "Email",
+                      controller: _emailController,
+                      obscureText: false),
+                  const SizedBox(height: 20),
+                  InputWidget(
+                      hintText: "Password",
+                      controller: _passwordController,
+                      obscureText: true)
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
